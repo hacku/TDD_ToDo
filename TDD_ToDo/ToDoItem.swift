51bd2c174a9097b23ecf9580ad26b24a6fcd2574
Philipp Hackbarth <philipphackbarth@icloud.com>
2019-09-20 16:14:51 +0200

    initial commit

---
+++ TDD_ToDo/ToDoItem.swift
@@ -0,0 +1,13 @@
+//
+//  ToDoItem.swift
+//  TDD_ToDo
+//
+//  Created by Philipp Hackbarth on 20.09.19.
+//  Copyright © 2019 HGFX. All rights reserved.
+//
+
+import Foundation
+
+struct ToDoItem {
+	let title: String
+}
--- TDD_ToDoTests/ToDoItemTests.swift
+++ TDD_ToDoTests/ToDoItemTests.swift
@@ -1,5 +1,5 @@
 //
-//  TDD_ToDoTests.swift
+//  ToDoItemTests.swift
 //  TDD_ToDoTests
 //
 //  Created by Philipp Hackbarth on 20.09.19.
@@ -7,9 +7,10 @@
 //
 
 import XCTest
+
 @testable import TDD_ToDo
 
-class TDD_ToDoTests: XCTestCase {
+class ToDoItemTests: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -19,16 +20,8 @@ class TDD_ToDoTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+	func test_Init_TakesTitle() {
+		ToDoItem(title: "Foo")
+	}
 
 }
