a95a146cd8f9447e58a8eca8c8efa0a2224722b6
Philipp Hackbarth <philipphackbarth@icloud.com>
2019-09-20 15:08:59 +0200

    Initial Commit

---
+++ TDD_ToDo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TDD_ToDo
+//
+//  Created by Philipp Hackbarth on 20.09.19.
+//  Copyright © 2019 HGFX. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
---
+++ TDD_ToDoTests/TDD_ToDoTests.swift
@@ -0,0 +1,34 @@
+//
+//  TDD_ToDoTests.swift
+//  TDD_ToDoTests
+//
+//  Created by Philipp Hackbarth on 20.09.19.
+//  Copyright © 2019 HGFX. All rights reserved.
+//
+
+import XCTest
+@testable import TDD_ToDo
+
+class TDD_ToDoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
